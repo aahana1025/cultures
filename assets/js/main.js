df2fdf986dece4cd104bd416f467aae4c5b8e7be
aahanasharma <293250009+aahana1025@users.noreply.github.com>
2026-06-24 16:53:41 -0500

    second commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,7 @@
 // main JS
 document.addEventListener('DOMContentLoaded',()=>{console.log('Project loaded')})
+function text(e){
+    coordinate.setText("(" + e.getX() + "," + e.getY() + ")");
+    coordinate.setPosition(e.getX(), e.getY());
+    coordinate.setColor(Color.black);
+}
