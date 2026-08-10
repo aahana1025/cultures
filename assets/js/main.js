d7610d3e422f98b9c67aad63d235d607874fccaa
chakrichandu043 <chakrichandu043@gmail.com>
2026-08-10 02:40:26 -0500

    js setup

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,7 @@
 // main JS
 document.addEventListener('DOMContentLoaded',()=>{console.log('Project loaded')})
-console.log("JS is running!");
 
-const img = document.getElementById("map");
-const output = document.getElementById("output");
-
-// Guard against missing elements so script doesn't throw
-if (img && output) {
-    img.addEventListener("mousemove", (e) => {
-        const rect = img.getBoundingClientRect();
-        const x = e.clientX - rect.left;
-        const y = e.clientY - rect.top;
-        output.innerText = `X: ${Math.floor(x)}, Y: ${Math.floor(y)}`;
-    });
-} else {
-    console.warn('Map image or output element not found', { img, output });
-}
-var coordinate;
+alert("MAIN.JS IS WORKING!");
 
 
 //const img = document.querySelector("img");
